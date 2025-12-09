9b4b8bc374ec53d8d1bcdbd737467a091213db4b
Samuel Ignacio Verazay <samu_vm@hotmail.com.ar>
2025-12-09 03:32:36 -0300

    Agregando la linea

--- IngSw-Tfi.Domain/Repository/ISocialWorkRepository.cs
+++ IngSw-Tfi.Domain/Repository/ISocialWorkRepository.cs
@@ -5,6 +5,7 @@ namespace IngSw_Tfi.Domain.Repository;
 public interface ISocialWorkRepository
 {
     Task<List<SocialWork>?> GetAll();
+    void AddSocialWork(SocialWork newSocialWork);
     bool ValidateInsuranceAndMember(string nameSocial, int memerNumber);
     Task<SocialWork?> GetById(string idSocialWork);
 }
